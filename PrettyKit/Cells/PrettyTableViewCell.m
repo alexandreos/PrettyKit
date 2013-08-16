ca5aea898bd8b5c23c1a74373513d3a86f58acae
Alexandre <alexandre_o_s@yahoo.com>
2013-08-16 12:38:24 -0300

    Fixing issue #34
    Fixed support for cell instantiation from storyboard.

--- PrettyKit/Cells/PrettyTableViewCell.m
+++ PrettyKit/Cells/PrettyTableViewCell.m
@@ -398,6 +398,30 @@ typedef enum {
     self.customSeparatorStyle = UITableViewCellSeparatorStyleSingleLine;
 }
 
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self)
+    {
+        [self.contentView addObserver:self forKeyPath:@"frame" options:NSKeyValueObservingOptionOld context:nil];
+        
+        PrettyTableViewCellBackground *bg = [[PrettyTableViewCellBackground alloc] initWithFrame:self.frame
+                                                                                        behavior:CellBackgroundBehaviorNormal];
+        bg.cell = self;
+        self.backgroundView = bg;
+        [bg release];
+        
+        bg = [[PrettyTableViewCellBackground alloc] initWithFrame:self.frame
+                                                         behavior:CellBackgroundBehaviorSelected];
+        bg.cell = self;
+        self.selectedBackgroundView = bg;
+        [bg release];
+        
+        [self initializeVars];
+    }
+    return self;
+}
+
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
